352747643e2756c0b865cb548fdee394f1a3758a
Anna Rogers <2muddybarefeet@gmail.com>
2016-07-24 13:28:47 -0700

    initial commit with core data models added

---
+++ Note.swift
@@ -0,0 +1,18 @@
+//
+//  Note.swift
+//  CoolNotes
+//
+//  Created by Anna Rogers on 7/24/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Note: NSManagedObject {
+
+    //init
+    
+
+}
---
+++ Notebook.swift
@@ -0,0 +1,17 @@
+//
+//  Notebook.swift
+//  CoolNotes
+//
+//  Created by Anna Rogers on 7/24/16.
+//  Copyright © 2016 Anna Rogers. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Notebook: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
